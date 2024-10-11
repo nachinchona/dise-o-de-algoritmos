cfa9428564ee8483056c9753dd2d8fd45b486269
nachinchona <ignacio.navarro@est.fi.uncoma.edu.ar>
2024-10-11 16:11:16 -0300

    probando ando

---
+++ tp1/Estructuras/src/StableParing/Main.java
@@ -0,0 +1,15 @@
+package StableParing;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] h = { "Carlos", "Ricardo", "Pepe", "Eric", "Rene" };
+        String[] m = { "Carla", "Erica", "Penelope", "Paula", "Rocio" };
+        int[][] ph = { { 3, 2, 0, 1, 4 }, { 4, 3, 0, 1, 2 }, { 1, 4, 0, 3, 2 }, { 0, 2, 4, 3, 1 }, { 1, 2, 0, 3, 4 } };
+        int[][] pm = { { 1, 0, 2, 3, 4 }, { 2, 4, 3, 0, 1 }, { 4, 0, 2, 3, 1 }, { 1, 0, 3, 4, 2 }, { 0, 4, 3, 2, 1 } };
+        int[][] pp = { { 1, 2 }, { 4, 0 } };
+        StableParing p = new StableParing(h, m, ph, pm, pp);
+        p.matchear();
+        System.out.println(p.toString());
+        System.out.println(p.esParejaProhibida(4, 0));
+    }
+}
