92b2abb5faa3094692ad33c47c569290494cd12c
nachinchona <ignacio.navarro@est.fi.uncoma.edu.ar>
2024-10-11 16:35:25 -0300

    hashing

--- tp1/Estructuras/src/Varios/Hashing.java
+++ tp1/Estructuras/src/Varios/Hashing.java
@@ -1,31 +1,23 @@
 package Varios;
 
-import java.io.File;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
 
-
 public class Hashing {
     public static void main(String[] args) {
-        File file = new File("texto.txt");
-        System.out.println(file);
         HashMap<String, Integer> mapeo = new HashMap<>();
-        String texto = "Hola a todos los que estén leyendo este texto. Este texto es utilizado para el algoritmo de Hashing en la carpeta Varios. Lee a todas las palabras repetidas y toma la cantidad de ocurrencias de cada una en un HashMap.";
+        String texto = "";
         
-        Path path = Path.of("texto.txt");
+        Path path = Path.of("recursos/texto.txt");
 
         try {
-            texto = Files.readString(file.toPath(), StandardCharsets.ISO_8859_1);
+            texto = Files.readString(path, StandardCharsets.ISO_8859_1);
         } catch (Exception e) {
             System.out.println(e);
         }
-        String[] palabras = texto.split("(?=[,.])|\\s+");
-
-        for (int i = 0; i < palabras.length; i++) {
-            System.out.println(palabras[i]);
-        }
+        String[] palabras = texto.split("[\\s.,]+");
 
         for (int i = 0; i < palabras.length; i++) {
             if (mapeo.containsKey(palabras[i])) {
@@ -35,6 +27,8 @@ public class Hashing {
             }
         }
 
-        System.out.println("cant de 'de':" + mapeo.get("de"));
+        for (String key : mapeo.keySet()) {
+            System.out.println("Cant. ocurrencias de "+key+":" + mapeo.get(key));
+        }
     }
 }
